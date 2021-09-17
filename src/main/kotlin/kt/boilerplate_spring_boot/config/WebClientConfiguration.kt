c63d48babaad0e5cab6a946d31f4447d5dfb43ac
odilio <odilionoronha@gmail.com>
2021-09-17 10:47:21 -0300

    adição de autenticação por keycloak

--- src/main/kotlin/kt/boilerplate_spring_boot/config/WebClientConfiguration.kt
+++ src/main/kotlin/kt/boilerplate_spring_boot/config/WebClientConfiguration.kt
@@ -7,12 +7,10 @@ import kt.boilerplate_spring_boot.service.WebClientService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.Connection
 import reactor.netty.http.client.HttpClient
 import reactor.netty.tcp.TcpClient
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 @Configuration
